370e55cfb64e383c1b550c8fb528ce0dd4494546
Devesh_singh <choudharydevesh121@gmail.com>
2023-11-14 02:12:44 +0530

    added all necessery auth and database configuration tasks

---
+++ src/appWrite/authService.js
@@ -0,0 +1,82 @@
+/* eslint-disable no-useless-catch */
+import { Account, ID, Client } from "appwrite";
+import conf from "../conf/conf";
+
+//***** for better approach we are using the class based components practices */
+export class AuthService {
+    client = new Client();
+    account;
+
+    //***** authService(line:33) object call hone per humara constructor call hoga jiske ander hum account ki functionality rakhenge  */
+    constructor() {
+        this.client
+            .setEndpoint(conf.appWrite_url)
+            .setProject(conf.appWrite_ProjectId);
+
+        //******* ab jab humare paas client ki values aa gai hai to hum use account me bhej denge  */
+        this.account = new Account(this.client);
+    }
+
+    //******** ab hum koi bhi backend service pe dependencies nahi chahte iske liye hum ek rapper bana rahe hai taki hum jab bhi chahe koi bhi backend service use kar sake chahe vo appwrite ,firebase ya fir apna khud ka database  */
+    //********* SignUp method below */
+    async createAccount({ name, email, password }) {
+        // eslint-disable-next-line no-useless-catch
+        try {
+            //******** hum wait karenge ek account create hone ka aur account create hoga account (line:7) variable se  */
+            const userAccount = await this.account.create(
+                ID.unique(),
+                name,
+                email,
+                password
+            );
+            //****** ab agar useracount taiyar ho gya ho to hum use login karwa denge :point_down:  */
+            if (userAccount) {
+                //call another method
+                return this.login({ email, password })
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            //   console.log(error);
+            throw error;
+        }
+    }
+    //********* login method below */
+    async login({ email, password }) {
+
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+
+    }
+
+    async getCurrentUser() {
+        try {
+           return await this.account.get()
+
+        } catch (error) {
+            console.log("Appwrite service  ::  getCurrentUser ::  error ",error);
+            throw error;
+        }
+        // eslint-disable-next-line no-unreachable
+        return null;
+    }
+
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite service  ::  logout ::  error ",error);
+        }
+    }
+
+
+}
+
+//****  ab hume iss class ko use karne ke liye objects banane padenge  */
+const authService = new AuthService();
+
+export default authService; //**** exporting the class */
---
+++ src/appWrite/databaseConfig.js
@@ -0,0 +1,144 @@
+/* eslint-disable no-useless-catch */
+import { Client, Databases, ID, Query, Storage } from "appwrite";
+import conf from "../conf/conf";
+
+export class DbService {
+    client = new Client();
+    databases;
+    bucket;
+
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appWrite_url)
+            .setProject(conf.appWrite_ProjectId);
+
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client)
+    }
+
+    async createPost({ title, userId, content, slug, featuredImage, status }) {
+        try {
+            //*******databases.createDocument('[DATABASE_ID]', '[COLLECTION_ID]', '[DOCUMENT_ID]', {}); */
+            return await this.databases.createDocument(
+                conf.appWrite_DatabaseId,
+                conf.appWrite_CollactionId,
+                slug,
+                {
+                    title, content, featuredImage, status, userId
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service  ::  createPost ::  error ", error);
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(
+                conf.appWrite_DatabaseId,
+                conf.appWrite_CollactionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage, status,
+                }
+
+            )
+        } catch (error) {
+            console.log("Appwrite service  ::  updatePost ::  error ", error);
+        }
+
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                conf.appWrite_DatabaseId,
+                conf.appWrite_CollactionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite service  ::  deletePost ::  error ", error)
+            return false;
+        }
+    }
+
+    async getAllPost(queries) {
+        try {
+            return await this.databases.listDocuments(
+                conf.appWrite_DatabaseId,
+                conf.appWrite_CollactionId,
+                queries,
+                [
+                    Query.equal("status", "active")
+                ]
+            )
+
+        } catch (error) {
+            console.log("Appwrite service  ::  getAllPost ::  error ", error);
+            return false
+
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.listDocument(
+                conf.appWrite_DatabaseId,
+                conf.appWrite_CollactionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite service  ::  getPost ::  error ", error);
+            return false;
+        }
+    }
+
+    //********* upload files */
+
+    async uploadFile(file) {
+        try {
+            await this.bucket.createFile(
+                conf.appWrite_BucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service  ::  uploadfile ::  error ", error);
+            return false;
+        }
+    }
+
+    //********** createFile method ko call karne ke baad hume fileId mil jayegi aur file delete hone ke baad hum status true kar denge */
+    async deleteFile(fileId) {
+        try {
+            await this.Storage.deleteFile(
+                conf.appWrite_BucketId,
+                fileId
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite service  ::  deletefile ::  error ", error);
+            return false;
+        }
+    }
+
+    //****** here no need to call async-await because its already very fast  */
+    async getFilePreview(fileId) {
+        try {
+            return await this.bucket.getFilePreview(
+                conf.appWrite_BucketId,
+                fileId
+            )
+        } catch (error) {
+            console.log("Appwrite service  ::  uploadfile ::  error ", error);
+            return false;
+        }
+    }
+}
+
+const dbService = new DbService
+export default dbService
